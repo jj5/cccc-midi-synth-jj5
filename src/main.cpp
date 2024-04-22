eb57dde7751a877873529f4414209198b5db5809
John Elliot V <jj5@jj5.net>
2024-04-23 02:12:58 +1000

    Work, work...

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+// 2024-04-23 jj5 - baud rate for serial communication
+//
+#define BAUD_RATE 115200
+
 // 2024-04-23 jj5 - ASCII control characters we support
 //
 #define ASCII_BS 8
@@ -14,8 +18,16 @@
 
 // 2024-04-23 jj5 - pin assignments
 //
-int led_pin = 13;
-int pwm_pin = 9;
+#define LED_PIN 13
+#define PWM_PIN 9
+
+// 2024-04-23 jj5 - strings
+//
+const char *STR_EMPTY = "";
+const char *STR_READY = "Ready.";
+const char *STR_COMMAND_PROMPT = "Command: ";
+const char *STR_COMMAND_TOO_LONG = "Command too long.";
+const char *STR_UNKNOWN_COMMAND = "Unknown command.";
 
 // 2024-04-23 jj5 - LED control commands
 //
@@ -28,6 +40,7 @@ const char *CMD_FLASH = "flash";
 const char *CMD_START = "start";
 const char *CMD_STOP  = "stop";
 
+//
 // 2024-04-23 jj5 - LED states
 //
 
@@ -35,7 +48,9 @@ enum led_state { LED_OFF, LED_ON, LED_FLASH };
 
 volatile enum led_state led_state = LED_OFF;
 
+//
 // 2024-04-23 jj5 - we use timer/counter2 to toggle the LED every 500 ms
+//
 
 volatile int timer_2_counter = 0;
 
@@ -45,31 +60,44 @@ ISR( TIMER2_COMPA_vect ) {
     timer_2_counter = 0;
     // 2024-04-22 jj5 - toggle the LED every 500 ms
     if ( led_state == LED_FLASH ) {
-      int pin = digitalRead( led_pin );
-      digitalWrite( led_pin, !pin );
+      int pin = digitalRead( LED_PIN );
+      digitalWrite( LED_PIN, !pin );
     }
   }
 }
 
 void setup_timer_2() {
 
-  // Set up Timer/Counter2 for CTC mode
-  TCCR2A = 0x02;  // Set to CTC Mode
-  TCCR2B = 0x07;  // Use a prescaler of 1024
+  // 2024-04-23 jj5 - set up Timer/Counter2 for CTC mode
 
-  // Calculate OCR2A value for 500 ms delay
-  // Timer frequency = 16MHz / 1024 = 15625 Hz
-  // Ticks needed = 15625 * 0.5s = 7812.5
-  // Since Timer2 is 8-bit, and max value is 255, we use an additional counter
-  OCR2A = 156;    // (7812 / 50) - 1 approximately
+  // 2024-04-23 jj5 - set to CTC Mode
+  //
+  TCCR2A = 0x02;
 
-  TIMSK2 = 0x02;  // Enable Timer2 Compare Match A interrupt
+  // 2024-04-23 jj5 - use a prescaler of 1024
+  //
+  TCCR2B = 0x07;  
+
+  // 2024-04-23 jj5 - to calculate OCR2A value for 500 ms delay:
+  // timer frequency = 16MHz / 1024 = 15625 Hz
+  // ticks needed = 15625 * 0.5s = 7812.5
+  // (7812 / 50) - 1 = 156 approximately
+  //
+  OCR2A = 156;
+
+  // 2024-04-23 jj5 - enable Timer2 Compare Match A interrupt
+  //
+  TIMSK2 = 0x02;
   
-  sei();          // Enable global interrupts
+  // 2024-04-23 jj5 - enable global interrupts
+  //
+  sei();
 
 }
 
+//
 // 2024-04-23 jj5 - we use timer/counter1 to generate a 261.63 Hz tone (middle C)
+//
 
 void timer_1_stop() {
   TCCR1A = 0;
@@ -78,44 +106,45 @@ void timer_1_stop() {
 
 void timer_1_start() {
 
-  // Stop the timer
+  // 2024-04-23 jj5 - stop the timer
+  //
   timer_1_stop();
 
-  // Set the mode to Fast PWM, TOP in ICR1
+  // 2024-04-23 jj5 - set the mode to Fast PWM, TOP in ICR1
+  //
   TCCR1A |= (1 << WGM11);
   TCCR1B |= (1 << WGM12) | (1 << WGM13);
 
-  // Set the prescaler to 8
+  // 2024-04-23 jj5 - set the prescaler to 8
+  //
   TCCR1B |= (1 << CS11);
 
-  // Calculate the value of ICR1 for the desired frequency (261.63 Hz, middle C)
+  // 2024-04-23 jj5 - calculate the value of ICR1 for the desired frequency (261.63 Hz, middle C)
+  //
   unsigned int icr1 = (16000000 / (8 * 261.63)) - 1;
-
   ICR1 = icr1;
 
-  // Set OCR1A for a 50% duty cycle
+  // 2024-04-23 jj5 - set OCR1A for a 50% duty cycle
+  //
   OCR1A = icr1 / 2;
 
-  // Connect PWM output to pin OC1A (pin 9 on most Arduinos)
+  // 2024-04-23 jj5 - connect PWM output to pin OC1A (pin 9 on most Arduinos)
+  //
   TCCR1A |= (1 << COM1A1);
 
 }
 
 void setup_timer_1() {
-
-  // Set the pin you will use for the PWM output
-  pinMode( pwm_pin, OUTPUT );
-
   timer_1_start();
-
 }
 
 void setup() {
-  pinMode( led_pin, OUTPUT );
+  pinMode( LED_PIN, OUTPUT );
+  pinMode( PWM_PIN, OUTPUT );
   setup_timer_2();
   setup_timer_1();
-  Serial.begin( 115200 );
-  Serial.println( "Ready." );
+  Serial.begin( BAUD_RATE );
+  Serial.println( STR_READY );
 }
 
 bool is_control_char( char c ) { return c < 32 || c == 127; }
@@ -127,7 +156,7 @@ void read_command() {
 
   //char debug_buffer[ CMD_LEN ];
 
-  Serial.print( "Command: " );
+  Serial.print( STR_COMMAND_PROMPT );
 
   for ( ;; ) {
 
@@ -148,7 +177,7 @@ void read_command() {
             }
             break;
           case ASCII_CR :
-            Serial.println( "" );
+            Serial.println( STR_EMPTY );
             command_buffer[ command_index ] = '\0';
             command_index = 0;
             if ( strcmp( command_buffer, CMD_ON ) == 0 ) {
@@ -167,7 +196,7 @@ void read_command() {
               timer_1_stop();
             }
             else {
-              Serial.println( "Unknown command." );
+              Serial.println( STR_UNKNOWN_COMMAND );
             }
             return;
         }
@@ -182,9 +211,9 @@ void read_command() {
 
         if ( command_index >= CMD_LEN ) {
           command_index = 0;
-          Serial.println( "" );
-          Serial.println( "Command too long." );
-          Serial.print( "Command: " );
+          Serial.println( STR_EMPTY );
+          Serial.println( STR_COMMAND_TOO_LONG );
+          Serial.print( STR_COMMAND_PROMPT );
         }
       }
     }
@@ -195,13 +224,13 @@ void loop() {
   read_command();
   switch ( led_state ) {
     case LED_ON :
-      digitalWrite( led_pin, HIGH );
+      digitalWrite( LED_PIN, HIGH );
       break;
     case LED_OFF :
-      digitalWrite( led_pin, LOW );
+      digitalWrite( LED_PIN, LOW );
       break;
     case LED_FLASH :
-      // 2024-04-22 jj5 - this is handled in the interrupt
+      // 2024-04-22 jj5 - this is handled in the interrupt handler for timer/counter2
       break;
   }
 }
